ec886832dc29468424c72551b268b6232717c2c1
fengqiao“git commit -m first <1404369980@qq.com>
2020-01-02 17:26:29 +0800

    sd

---
+++ src/main/java/com/zuul/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author 易桥锋
+ * @version 1.0
+ * @date 2020/1/2 0002 17:06
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
